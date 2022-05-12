7edada821c41e5fe90dcdfdc8ef59057eab20df0
BlendiSpahiu <blendi.spahiu@ornio.no>
2022-05-12 13:12:42 +0200

    Added netlify cms

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -6,21 +6,23 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <Meta />
         <Links />
       </head>
+
       <body>
         <Outlet />
         <ScrollRestoration />
